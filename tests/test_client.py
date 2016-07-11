e0fc7e191d77c72fb2c86ea6487bb2446cfa378f
Miro Hrončok <miro@hroncok.cz>
2016-07-11 21:32:08 +0200

    Use betamax pytest fixture

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,9 @@
 import pytest
 from betamax import Betamax
-from requests import Session
 
 from octoclient import OctoClient
 
 
-SESSION = Session()
 URL = 'http://printer15.local'
 APIKEY = 'YouShallNotPass'
 
@@ -13,13 +11,12 @@ with Betamax.configure() as config:
     config.cassette_library_dir = 'tests/fixtures/cassettes'
 
 
+@pytest.mark.usefixtures('betamax_session')
 class TestClient:
-    def test_init_works_with_good_auth(self):
-        with Betamax(SESSION).use_cassette('init_works_with_good_auth'):
-            # Should not raise anything
-            oc = OctoClient(url=URL, apikey=APIKEY, session=SESSION)
+    def test_init_works_with_good_auth(self, betamax_session):
+        # Should not raise anything
+        oc = OctoClient(url=URL, apikey=APIKEY, session=betamax_session)
 
-    def test_init_raises_with_bad_auth(self):
-        with Betamax(SESSION).use_cassette('init_raises_with_bad_auth'):
-            with pytest.raises(RuntimeError):
-                oc = OctoClient(url=URL, apikey='nope', session=SESSION)
+    def test_init_raises_with_bad_auth(self, betamax_session):
+        with pytest.raises(RuntimeError):
+            oc = OctoClient(url=URL, apikey='nope', session=betamax_session)
