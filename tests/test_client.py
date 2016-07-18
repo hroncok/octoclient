043413e767060779fb386f410b05c441b7cdbf03
Miro Hrončok <miro@hroncok.cz>
2016-07-18 13:40:52 +0200

    Don't assign to unused variables

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,8 +15,8 @@ with Betamax.configure() as config:
 class TestClient:
     def test_init_works_with_good_auth(self, betamax_session):
         # Should not raise anything
-        oc = OctoClient(url=URL, apikey=APIKEY, session=betamax_session)
+        OctoClient(url=URL, apikey=APIKEY, session=betamax_session)
 
     def test_init_raises_with_bad_auth(self, betamax_session):
         with pytest.raises(RuntimeError):
-            oc = OctoClient(url=URL, apikey='nope', session=betamax_session)
+            OctoClient(url=URL, apikey='nope', session=betamax_session)
